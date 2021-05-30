76952facd54a73fc348f511b82823891e8569e15
Suraj Singh <surajsingh220698@outlook.com>
2021-05-30 11:55:01 +0530

    Add ids to different meal components.

--- src/Meals/MealsItem/MealItem.js
+++ src/Meals/MealsItem/MealItem.js
@@ -14,7 +14,7 @@ const MealItem = props => {
         <div className={styles.price}>{price}</div>
       </div>
       <div>
-        <MealItemForm />
+        <MealItemForm id={props.id} />
       </div>
     </li>
   );
