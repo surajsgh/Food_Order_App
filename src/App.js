c827d33938ad5e4bf8519740dc5741f89c0ada42
Suraj Singh <surajsingh220698@outlook.com>
2021-05-30 15:14:52 +0530

    Add a shopping cart component and a modal via a react portal.

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React, { Fragment } from 'react';
 
 import Header from '../src/Layout/Header';
 import Meals from '../src/Meals/Meals';
+import Cart from './Cart/Cart';
 
 function App() {
   return (
     <Fragment>
+      <Cart />
       <Header />
       <main>
         <Meals />
