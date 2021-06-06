c0a54486ab31219ff4219e23660b4eb4a13cb68a
Suraj Singh <surajsingh220698@outlook.com>
2021-06-06 15:12:03 +0530

    Read the values from checkout form using useRef hook.

--- src/Cart/Checkout.js
+++ src/Cart/Checkout.js
@@ -1,28 +1,46 @@
-import React from 'react';
+import React, { useRef } from 'react';
 
 import styles from './Checkout.module.css';
 
 const Checkout = props => {
+  const nameInputRef = useRef();
+  const streetInputRef = useRef();
+  const postalCodeRef = useRef();
+  const cityCodeRef = useRef();
+
+  const submitHandler = event => {
+    event.preventDefault();
+
+    enteredName = nameInputRef.current.value;
+    enteredStreet = streetInputRef.current.value;
+    enteredPostal = postalCodeRef.current.value;
+    enteredCity = cityCodeRef.current.value;
+  };
+
   return (
-    <form>
+    <form className={styles.form} onSubmit={submitHandler}>
       <div className={styles.control}>
         <label htmlFor="name">Your name:</label>
-        <input type="text" id="name" />
+        <input type="text" id="name" ref={nameInputRef} />
       </div>
       <div className={styles.control}>
         <label htmlFor="street">Street:</label>
-        <input type="text" id="street" />
+        <input type="text" id="street" ref={streetInputRef} />
       </div>
       <div className={styles.control}>
         <label htmlFor="postal">Postal code:</label>
-        <input type="text" id="postal" />
+        <input type="text" id="postal" ref={postalCodeRef} />
       </div>
       <div className={styles.control}>
         <label htmlFor="city">City:</label>
-        <input type="text" id="city" />
+        <input type="text" id="city" ref={cityCodeRef} />
       </div>
-      <button onClick={props.onClose}>Cancel</button>
-      <button>Submit</button>
+      <div className={styles.actions}>
+        <button type="button" onClick={props.onClose}>
+          Cancel
+        </button>
+        <button className={styles.submit}>Submit</button>
+      </div>
     </form>
   );
 };
