0c2980606e2c0192779d2da790804fba2edc8895
Suraj Singh <surajsingh220698@outlook.com>
2021-06-06 11:55:47 +0530

    Retrieve data from firebase to the UI.

--- src/Meals/AvailableMeals.js
+++ src/Meals/AvailableMeals.js
@@ -1,38 +1,43 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import styles from './AvailableMeals.module.css';
 import Card from '../UI/Card';
 import MealItem from './MealsItem/MealItem';
 
-const DUMMY_MEALS = [
-  {
-    id: 'm1',
-    name: 'Sushi',
-    description: 'Finest fish and veggies',
-    price: 22.99,
-  },
-  {
-    id: 'm2',
-    name: 'Schnitzel',
-    description: 'A german specialty!',
-    price: 16.5,
-  },
-  {
-    id: 'm3',
-    name: 'Barbecue Burger',
-    description: 'American, raw, meaty',
-    price: 12.99,
-  },
-  {
-    id: 'm4',
-    name: 'Green Bowl',
-    description: 'Healthy...and green...',
-    price: 18.99,
-  },
-];
-
 const AvailableMeals = () => {
-  const meals = DUMMY_MEALS.map(meal => (
+  const [meals, setMeals] = useState([]);
+
+  useEffect(() => {
+    const loadData = async function () {
+      try {
+        const res = await fetch(
+          'https://food-order-application-8d176-default-rtdb.firebaseio.com/meals.json'
+        );
+        if (!res.ok) {
+          throw new Error('Something went wrong!');
+        }
+
+        const data = await res.json();
+
+        const loadedMeals = [];
+
+        for (const key in data) {
+          loadedMeals.push({
+            id: key,
+            name: data[key].name,
+            description: data[key].description,
+            price: data[key].price,
+          });
+        }
+        setMeals(loadedMeals);
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+    loadData();
+  }, []);
+
+  const reactMeals = meals.map(meal => (
     <MealItem
       id={meal.id}
       key={meal.id}
@@ -45,7 +50,7 @@ const AvailableMeals = () => {
   return (
     <section className={styles.meals}>
       <Card>
-        <ul>{meals}</ul>
+        <ul>{reactMeals}</ul>
       </Card>
     </section>
   );
